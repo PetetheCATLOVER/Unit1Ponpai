c875867e17f44d53cb9e617d64ec61e63419a1f9
SeanJ07 <91712813+SeanJ07@users.noreply.github.com>
2025-03-12 09:54:19 -0700

    finished 1.2
    finished 1.2

--- U1PlayerControlPonpai/Assets/Scripts/PlayerController.cs
+++ U1PlayerControlPonpai/Assets/Scripts/PlayerController.cs
@@ -11,8 +11,10 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    public float speed = 20;
     void Update()
     {
-        
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
